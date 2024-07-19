40308cfa19e9d1751833ddb5516d5e96175e5b16
D-H0f <devonoffutt@gmail.com>
2024-07-19 15:27:45 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import logging, logging.config
+from pathlib import Path
+from json import load
+
+
+def config_logging() -> logging.Logger:
+    cur_dir = Path(__file__).parent.resolve()
+    config_file = f"{cur_dir}/configs/logging_config.json"
+    with open(config_file) as filein:
+        config_settings = load(filein)
+    logging.config.dictConfig(config_settings)
+
+    return logging.getLogger(__file__)
+
+
+def main() -> None:
+    logger.info("testing info message")
+    logger.debug("testing debug message")
+    logger.warning("testing warn message")
+    logger.error("testing error message")
+    logger.exception("testing exception message")
+    logger.critical("testing critical message")
+
+
+if __name__ == "__main__":
+    logger = config_logging()
+    main()
